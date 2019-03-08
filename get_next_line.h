a1aaab5dee75ee3387fb752a1c5241196df1fd34
Zhenya Mamenko <emamenko@student.42.us.org>
2019-03-08 10:23:54 -0800

    buff_size

--- get_next_line.h
+++ get_next_line.h
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: emamenko <emamenko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: emamenko <emamenko@student.42.us.org>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/17 11:18:24 by emamenko          #+#    #+#             */
-/*   Updated: 2019/02/20 22:22:17 by emamenko         ###   ########.fr       */
+/*   Updated: 2019/03/07 19:56:14 by emamenko         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# define BUFF_SIZE 32
+# define BUFF_SIZE 1000
 
 typedef struct	s_fpt
 {
